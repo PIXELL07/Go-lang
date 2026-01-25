a3ff805056729bc6eaba6e9517b85c0e5aec8dfa
PIXELL07 <arunksn007@gmail.com>
2026-01-25 16:15:53 +0530

    pointers holds the memory address

--- 06mypointers/main.go
+++ 06mypointers/main.go
@@ -10,7 +10,7 @@ func main() {
 	var ptr = &myNumber
 
 	fmt.Println("value of actual pointer is ", ptr)
-	fmt.Println("value of pointer is ", *ptr)
+	fmt.Println("value of pointer is", *ptr)
 
 	*ptr = *ptr + 8
 	fmt.Println("new value is ", myNumber)
