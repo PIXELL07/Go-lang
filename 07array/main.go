6ed2c51146b66d3fcb43a631b2875b310811f967
PIXELL07 <arunksn007@gmail.com>
2026-01-25 16:24:17 +0530

    arrays

--- 07array/main.go
+++ 07array/main.go
@@ -8,6 +8,9 @@ func main() {
 
 	fruitList[0] = "Apple"
 	fruitList[1] = "Mango"
+	// with the help of len function we can find the length of array
+	// look we see the default value of string is empty string "" , there is no 2nd index value assigned yet
+	// but the length of array is still 4
 	fruitList[3] = "Banana"
 
 	fmt.Println("Fruit list is: ", fruitList)
